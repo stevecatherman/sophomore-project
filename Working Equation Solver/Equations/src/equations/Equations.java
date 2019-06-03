a46dba09011c3f769284d31a291904fdc0865f8f
cazam19 <mattpeace19@gmail.com>
2019-06-03 11:28:12 -0400

    Added two formulas
    D = xf - xi
    D = vi*t+1/2at^2

--- Working Equation Solver/Equations/src/equations/Equations.java
+++ Working Equation Solver/Equations/src/equations/Equations.java
@@ -10,10 +10,33 @@ package equations;
  * @author mpeace
  */
 public class Equations {
+    
+    //Calculates Displacement when given Initial and Final Position
+    public static String calculateDisplacement(String XF, String XI) { //Calculate Displacement. XF = final position, XF = initial 
+        String D;
+        double d;
+        double xf = Double.parseDouble(XF);//casting input XF as a double value using parse
+        double xi = Double.parseDouble(XI);//casting input XI as a double value using parse
+        d =  xf - xi;    //Displacement = Final Pos - Initial Pos
+        D = Double.toString(d);//casting d from double to string D for output
 
-    /**
-     * @param args the command line arguments
-     */
+        return D;//returns the string value for D
+    }
+    
+    //Calculates Displacement when given acceleration, Initial Velocity, and Time.
+    public static String calculateDisplacement2(String A, String VI, String T) { //Calculate Displacement. A = Acceleration, VI= Initial Velocity, T=Time
+        String D;
+        double d;
+        double a = Double.parseDouble(A);//casting input A as a double value using parse
+        double vi = Double.parseDouble(VI); //casting input VI as a double value using parse
+        double t = Double.parseDouble(T);//casting input T as a double value using parse
+        d = (vi*t)+(.5*a*Math.pow(t,2));//d = vi*t + (1/2)at^2
+
+        D = Double.toString(d);//casting d from double to string D for output
+
+        return D;//returns the string value for D
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
